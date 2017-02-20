0c14562004a95487174d171c4bdbfae512f30a4f
Sarvaritdinov Ravil <ra9oaj@gmail.com>
2017-02-20 17:43:24 +0700

    TK_GPage developing

--- lib/page.php
+++ lib/page.php
@@ -4,69 +4,79 @@
  * Class TK_GPage
  */
 class TK_GPage {
-	
+
 	/**
-     * WordPress Database Access Abstraction Object
-     * @var object
-     */
+	 * WordPress Database Access Abstraction Object
+	 * @var object
+	 */
 	protected $wpdb;
-	
+
 	/**
 	 * Current project index
 	 * @var integer
 	 */
 	protected $cur_project = 0;
-	
+
 	/**
 	 * Current page index
 	 * @var integer
 	 */
 	protected $cur_page = 0;
-	
+
 	/**
 	 * Max count projects on page
 	 * @var integer
 	 */
 	protected $max_projects = 0;
-	
+
 	/**
 	 * Max count links on page
 	 * @var integer
 	 */
 	protected $max_links = 0;
-	
+
+	/**
+	 * Last offset. This is needed for create next pages.
+	 * @var integer
+	 */
+	protected $last_offset = 0;
+
 	/**
 	 * A numerically indexed array of project row objects
 	 * @var array
 	 */
 	protected $projects;
-	
-	public function __construnct() {
+
+	public function __construct() {
 		global $wpdb;
-		
-		$this->wpdb = $wpdb;
-		$this->max_projects = 15;
-		$this->max_links = 10;
+
+		$this -> wpdb = $wpdb;
+		$this -> wpdb -> enable_nulls = true;
+		$this -> max_projects = 15;
+		$this -> max_links = 10;
 	}
-	
+
 	/**
 	 * Creates a list of projects for the page
-	 * @param $page_num integer
-	 * @param $ptype integer
+	 * @param $page_num integer Page Number
+	 * @param $ptype integer Page type
 	 */
 	public function createPage($page_num = 1, $ptype = 3) {
-		$offsrt = $page_num * $this->max_projects;
-		
-		$sql = "SELECT p.`ID`,
+		unset($this -> projects);
+		$offset = $this -> max_projects - $page_num * $this -> max_projects;
+		$user_id = get_current_user_id();
+		$prefix = $this -> wpdb -> prefix;
+
+		$sql = "SELECT p.`id`/*,
 					p.`post_author`,
 					p.`post_date`,
 					p.`post_title`,
 					p.`guid`,
 					pm1.meta_value as ptype,
-					pm2.meta_value as ptarget
-			FROM `{$this->wpdb->prefix}wp_posts` p,
-				 `{$this->wpdb->prefix}wp_postmeta` pm1,
-				 `{$this->wpdb->prefix}wp_postmeta` pm2
+					pm2.meta_value as ptarget*/
+			FROM `{$prefix}posts` p,
+				 `{$prefix}postmeta` pm1,
+				 `{$prefix}postmeta` pm2
 			WHERE pm1.post_id = p.id
 				AND pm2.post_id = p.id
 				AND p.`post_type` = 'tk_project'
@@ -75,18 +85,25 @@ class TK_GPage {
 				AND pm2.meta_key = 'ptarget'
 			ORDER BY p.`post_date` DESC
 			LIMIT %d OFFSET %d";
-			
-		$this->projects = $this->wpdb->get_results($this->wpdb->prepare($sql, 
-													  $ptype, 
-													  $this->cur_project, 
-													  $offset));
-		
-		/*foreach ($this->projects as $row) {
-			file_put_contents(__FILE__.".log", serialize($row)."\r\n", FILE_APPEND);
+
+		while (count($this -> projects) < $this -> max_projects) {
+			$res = $this -> wpdb -> get_results($this -> wpdb -> prepare($sql, $ptype, $this -> max_projects, $offset), OBJECT);
+
+			if (empty($res)) {
+				$this -> last_offset;
+				break;
+			}
+
+			foreach ($res as $row) {
+				$cur = new TK_GProject($row -> id);
+				++$offset;
+
+				if ($cur -> userCanRead($user_id)) {
+					$this -> projects[] = $cur;
+				}
+			}
 		}
-		file_put_contents(__FILE__.".log", "=====================================\r\n", FILE_APPEND);*/
 	}
 
-	    	
 };
 ?>
